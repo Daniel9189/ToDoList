11e83ac6bc5744facc9ad6a578306cf681ecd94f
Daniel Rocha de Brito <danielrocha.b155@gmail.com>
2026-02-16 18:29:12 -0300

    feat: getter/setter users wiht lombok

--- src/main/java/com/danielrocha/todolist/user/UserModel.java
+++ src/main/java/com/danielrocha/todolist/user/UserModel.java
@@ -1,38 +1,16 @@
 package com.danielrocha.todolist.user;
 
+import lombok.Data;
+
+
+@Data // Adiciona um getter e setter para todos os atributos da classe
+// @Getter para adicionar somente o get
+// @Setter para adicionar somente o set
 public class UserModel {
-    
     
     private String username;
     private String name;
     private String password;
-
-    /*Getters e Setters*/
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-    
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
 
 }
 
