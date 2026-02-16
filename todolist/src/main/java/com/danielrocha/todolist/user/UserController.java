a858a84684a8b3bf71e22f1d8fd47e6f5825ff11
Daniel Rocha de Brito <danielrocha.b155@gmail.com>
2026-02-16 17:19:29 -0300

    feat: get and set users

---
+++ todolist/src/main/java/com/danielrocha/todolist/user/UserController.java
@@ -0,0 +1,38 @@
+package com.danielrocha.todolist.user;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/*
+Modificadores:
+public
+private
+protected 
+*/ 
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    /*
+    Tipos de retorno:
+    String
+    Integer
+    Double
+    Float
+    Char
+    Date
+    Void
+    */
+    /*
+    Body
+    */
+
+    @PostMapping("/")
+    public void createUser(@RequestBody UserModel userModel) {
+        System.out.println(userModel.getUsername());
+    }
+}
