b3990c695338143ad1867bcffe0ebae43bdcdce0
Daniel Rocha de Brito <danielrocha.b155@gmail.com>
2026-02-18 20:41:22 -0300

    feat: user password validation

--- src/main/java/com/danielrocha/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/danielrocha/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,13 @@ package com.danielrocha.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.danielrocha.todolist.user.IUserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,13 +17,16 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter{
+    
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
         
         // Pegar a autenticação (usuário e senha)
-
         var authorization = request.getHeader("Authorization");
         var authEncoded = authorization.substring("Basic".length()).trim();
         
@@ -31,18 +38,22 @@ public class FilterTaskAuth extends OncePerRequestFilter{
         String username = credentials[0];
         String password = credentials[1];
 
-        System.out.println("Authorization");
-        System.out.println(username);
-        System.out.println(password);
-
-
         // Validar usuário
-        // Validar senha
-        // Deixar seguir
-        filterChain.doFilter(request, response);
-    }
+        var user = this.userRepository.findByUsername(username);
 
-    
-    
-    
+        if (user == null) {
+            response.sendError(401);
+        } else {
+
+            // Validar senha
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+            if (passwordVerify.verified) {
+                // Deixar seguir
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(401);
+            }
+        }
+    }
 }
